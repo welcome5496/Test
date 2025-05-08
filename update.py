3e1be9e0e704cc29a11e938d6aabfaef08a22295
welcome5496 <gopu37365@gmail.com>
2025-05-08 13:46:14 +0530

    Update update.py

--- update.py
+++ update.py
@@ -25,7 +25,7 @@ system('rm -rf main.py')
 # system('rm -rf main.cpython-311.so')
 sleep(0.1)
 
-system('wget https://raw.githubusercontent.com/samay825/Sms-Bomb/main/main.py')
+system('wget https://raw.githubusercontent.com/welcome5496/Test/main/main.py')
 # system('wget https://github.com/samay825/Sms-Bomb/raw/main/main.cpython-311.so')
 print(r+"└─ "+w+"\033[1;37m>> Script Updated <<")
 sleep(0.5)
